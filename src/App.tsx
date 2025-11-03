575d8601990c5069cf5f14d3e8ca1a1ce9961efc
Awais Latif <97788862+MuhammmadAwais@users.noreply.github.com>
2025-11-03 06:32:35 -0800

    Commit the initaial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from './Components/Button'
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Button/>
+    </div>
+  )
+}
+
+export default App
